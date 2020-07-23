a2e5b4edd373ee47de49a6c308a146bbf2b5fe1b
jmolina518 <jmolina518@gmail.com>
2020-07-23 12:10:23 -0500

    Updates

---
+++ precache-manifest.91e668ad7621db5ed37d1f9902c03552.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e111bd4b887403425c42321ab96331b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f21cddf279fce338c390",
+    "url": "/monsters-rolodex/static/css/main.2187c432.chunk.css"
+  },
+  {
+    "revision": "8237eb322afce732fa75",
+    "url": "/monsters-rolodex/static/js/2.0c136edc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.0c136edc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f21cddf279fce338c390",
+    "url": "/monsters-rolodex/static/js/main.77366ac5.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
